f9d9f9d35ce0fdbeb8d8c6a60e99fda9f77cb4bc
Naveenprasath1997 <108398934+Naveenprasath1997@users.noreply.github.com>
2025-07-01 16:59:44 +0530

    Update entrypoint.sh

--- bioinfo_pipeline/entrypoint.sh
+++ bioinfo_pipeline/entrypoint.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-
-# Exit on any error
 set -e
 
-# Name of your conda environment
 ENV_NAME="16_ppl_bioinfo-env"
-
-# Activate conda environment
 source /opt/conda/etc/profile.d/conda.sh
 conda activate "$ENV_NAME"
 
-# Echo environment info for debug
 echo "🧬 Conda environment '$ENV_NAME' activated."
 echo "📁 Working directory: $(pwd)"
 echo "🧾 Running command: $@"
 
-# If no command is given, start a shell
 if [ $# -eq 0 ]; then
     exec bash
 else
     exec "$@"
 fi
-
