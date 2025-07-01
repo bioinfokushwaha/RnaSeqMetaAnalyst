db668cdae23cb45a3087a631615caf13c0828209
Naveenprasath1997 <108398934+Naveenprasath1997@users.noreply.github.com>
2025-07-01 16:48:32 +0530

    Update entrypoint.sh

--- bioinfo_pipeline/entrypoint.sh
+++ bioinfo_pipeline/entrypoint.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-# entrypoint.sh
 
-# Activate conda env and run your command
-exec conda run --no-capture-output -n 16_ppl_bioinfo-env "$@"
+# Exit on any error
+set -e
+
+# Name of your conda environment
+ENV_NAME="rnaseq"
+
+# Activate conda environment
+source /opt/conda/etc/profile.d/conda.sh
+conda activate "$ENV_NAME"
+
+# Echo environment info for debug
+echo "🧬 Conda environment '$ENV_NAME' activated."
+echo "📁 Working directory: $(pwd)"
+echo "🧾 Running command: $@"
+
+# If no command is given, start a shell
+if [ $# -eq 0 ]; then
+    exec bash
+else
+    exec "$@"
+fi
+
